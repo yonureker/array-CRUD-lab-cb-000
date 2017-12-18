2fd5bf0c297c1e7c904c621a8b4870a09722d498
Onur Eker <yonureker@gmail.com>
2017-12-18 07:56:30 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,15 +3,15 @@ def create_an_empty_array
 end
 
 def create_an_array
-
+  myArray = [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-
+  element << array
 end
 
 def add_element_to_start_of_array(array, element)
-
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
